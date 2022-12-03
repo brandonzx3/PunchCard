e3a490263771981ba61748efc572d4399ad6fb32
Brandonzx3 <btherr2003@gmail.com>
2022-12-02 22:31:25 -0600

    fixed thing

--- src/state.js
+++ src/state.js
@@ -3,7 +3,7 @@ import { reactive } from "vue";
 const state = reactive({
     user: null,
     logged_in: false,
-    endpoint: "https://script.google.com/macros/s/AKfycbwepSTQTTjrAa1E1WSL9WqP5x0U-LdVxTrxlQvKPwIHlSkmKiZMAb5PQhcB4M2Y-9EP/exec"
+    endpoint: "https://script.google.com/macros/s/AKfycby1upfsDFH_cHdGqW0z2kF3pJ1eSgpxWo5bLXWokn4Q6Tk3Q5hKLMa2zppuPPwtwrxd/exec"
 });
 
 export default state;
