2394a1d40a3ceaa6ec045492222a2784d5acdf75
Brandonzx3 <btherr2003@gmail.com>
2023-01-21 00:49:02 -0600

    fixed checkin bug

--- src/state.js
+++ src/state.js
@@ -3,7 +3,7 @@ import { reactive } from "vue";
 const state = reactive({
     user: null,
     logged_in: false,
-    endpoint: "https://script.google.com/macros/s/AKfycby1XRNWGmvFIf-g_p9leZz_eJ_yx9bidrsbOYA7PkD6SzBOYWoeemGkiDRqUSQHHy2C/exec"
+    endpoint: "https://script.google.com/macros/s/AKfycbxrHqTO1ad3k7r1rP12eWwB0_60Ju0VMSiYTpVKdD0LTMQ5DpUC41wb8HdTLJD26wvi/exec"
 });
 
 export default state;
