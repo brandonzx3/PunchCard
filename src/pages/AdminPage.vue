9e4147417683eb04aecdd41016a00a67abd25690
brandonzx3 <btherr2003@gmail.com>
2025-01-27 22:04:21 -0600

    allow user to export user data sheet

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -10,13 +10,18 @@
     <div style="margin: 1em; display: flex; flex: row wrap; gap: 1em;">
         <q-btn color="red" @click="end_practice" :loading="!admin_puller.initial_load_finished">End Practice</q-btn>
     </div>
-    <q-table :loading="!admin_puller.initial_load_finished" title="Statistics" :columns=statistics_columns :rows=statistics_rows v-model:pagination="pagination" />
+    <q-table :loading="!admin_puller.initial_load_finished" title="Statistics" :columns=statistics_columns :rows=statistics_rows v-model:pagination="pagination">
+        <template v-slot:top-right>
+            <q-btn color="primary" icon-right="archive" label="Export to csv" no-caps @click="export_table" />
+      </template>
+    </q-table>
     
 </template>
 
 <script>
     import { defineComponent, reactive, ref } from 'vue';
     import { AdminPunchPuller, PB_People, PB_Punches } from "../state.js";
+    import { exportFile, Dialog, Notify } from 'quasar'
 
     export default defineComponent ({
         name: "Admin Panel",
@@ -118,12 +123,51 @@
 
         methods: {
             async end_practice() {
-                for (const [person_id, tracker] of this.admin_puller.trackers.entries()) {
+                Dialog.create({
+                    title: "Are You Sure!",
+                    message: "This Action Will Check Everyone Out. This Action Is Not Reversible",
+                    cancel: true,
+                }).onOk(async () => {
+                    for (const [person_id, tracker] of this.admin_puller.trackers.entries()) {
                     if(tracker.checked_in) {
                         await PB_Punches.create({
                             "person": person_id
                         });
                     }
+                }
+                })
+            },
+
+            
+            wrapCsvValue (val, formatFn, row) {
+                let formatted = formatFn !== void 0 ? formatFn(val, row) : val
+                formatted = formatted === void 0 || formatted === null ? '' : String(formatted)
+                formatted = formatted.split('"').join('""')
+                return `"${formatted}"`
+            },
+
+            export_table() {
+                const content = [this.statistics_columns.map(col => this.wrapCsvValue(col.label))].concat(
+                this.statistics_rows.map(row => this.statistics_columns.map(col => this.wrapCsvValue(
+                    typeof col.field === 'function'
+                    ? col.field(row)
+                    : row[ col.field === void 0 ? col.name : col.field ],
+                    col.format,
+                    row
+                )).join(','))).join('\r\n');
+
+                const status = exportFile(
+                    'table-export.csv',
+                    content,
+                    'text/csv'
+                );
+
+                if(status !== true) {
+                    Notify.create({
+                        message: 'Browser denied file download...',
+                        color: 'negative',
+                        icon: 'warning'
+                    });
                 }
             }
         }
