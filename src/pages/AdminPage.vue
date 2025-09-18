5ed677f45cbbf5cdeb06c6ad716b5af9c37778af
brandonzx3 <btherr2003@gmail.com>
2025-09-18 02:12:20 -0500

    added an add user form to the admin panel

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -9,6 +9,7 @@
     
     <div style="margin: 1em; display: flex; flex: row wrap; gap: 1em;">
         <q-btn color="red" @click="end_practice" :loading="!admin_puller.initial_load_finished">End Practice</q-btn>
+        <q-btn label="Add New User" @click="adduser = true" color="primary"/>
     </div>
     <q-table :loading="!admin_puller.initial_load_finished" title="Statistics" :columns=statistics_columns :rows=statistics_rows v-model:pagination="pagination">
         <template v-slot:top-right>
@@ -16,12 +17,51 @@
       </template>
     </q-table>
     
+    <q-dialog v-model="adduser">
+        <q-card style="width: 600px; max-width: 90vw;">
+            <q-card-section>
+                <div class="text-h6">User Information</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+                <q-form @submit.prevent="add_new_user" ref="userForm">
+                    <q-input
+                        v-model="new_user_id"
+                        type="number"
+                        label="User ID"
+                        class="q-mb-md"
+                        :rules="[val => !!val || 'User ID is required']"
+                    />
+                    <q-input
+                        v-model="new_user_first_name"
+                        label="First Name"
+                        class="q-mb-md"
+                        :rules="[val => !!val || 'First name is required']"
+                    />
+                    <q-input
+                        v-model="new_user_last_name"
+                        label="Last Name"
+                        class="q-mb-md"
+                        :rules="[val => !!val || 'Last name is required']"
+                    />
+
+                    <q-card-actions align="right">
+                        <q-btn flat label="Cancel" color="primary" @click="adduser = false" />
+                        <q-btn flat label="Add User" color="primary" type="submit" @click="add_new_user"/>
+                    </q-card-actions>
+                </q-form>
+            </q-card-section>
+        </q-card>
+    </q-dialog>
+
 </template>
 
 <script>
     import { defineComponent, reactive, ref } from 'vue';
     import { AdminPunchPuller, PB_People, PB_Punches } from "../state.js";
     import { exportFile, Dialog, Notify } from 'quasar'
+
+    const userForm = ref(null)
 
     export default defineComponent ({
         name: "Admin Panel",
@@ -58,6 +98,10 @@
 
         data() { return {
             admin_puller: new AdminPunchPuller(),
+            adduser: false,
+            new_user_id: null,
+            new_user_first_name: '',
+            new_user_last_name: '',
 
             people: reactive(new Map()),
 
@@ -136,6 +180,22 @@
                     }
                 }
                 })
+            },
+
+            async add_new_user() {
+                const data = {
+                    "person_id": this.new_user_id,
+                    "first_name": this.new_user_first_name.trim(),
+                    "last_name": this.new_user_last_name.trim()
+                }
+
+                const record = await PB_People.create(data);
+                
+                this.new_user_id = null;
+                this.new_user_first_name = '';
+                this.new_user_last_name = '';
+
+                this.adduser = false;
             },
 
             
